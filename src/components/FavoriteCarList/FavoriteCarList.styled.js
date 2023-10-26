ee3c282ae5e4c2d89b1a72bc33998d24faae5930
D3mk4 <dmtvchenko@gmail.com>
2023-10-26 16:07:18 +0300

    styled fixed

--- src/components/FavoriteCarList/FavoriteCarList.styled.js
+++ src/components/FavoriteCarList/FavoriteCarList.styled.js
@@ -3,14 +3,13 @@ import styled from 'styled-components';
 export const EmptyParkingContainer = styled.div`
   display: flex;
   flex-direction: column;
-  /* gap: 10px; */
   padding: 0 15px;
   height: calc(100vh - 140px);
   margin-top: -20px;
 
   & p {
     text-align: center;
-    font-size: clamp(28px, 2vw, 60px);
+    font-size: clamp(18px, 2vw, 60px);
 
     & span {
       color: var(--orange);
@@ -18,6 +17,6 @@ export const EmptyParkingContainer = styled.div`
   }
 
   & img {
-    max-height: 700px;
+    width: calc(100vh - 140px);
   }
 `;
